f9dfacc956256ba9ba8eaa1f2970a0eae867451d
Kamiya <58339640+kamiya10@users.noreply.github.com>
2024-06-05 20:03:24 +0800

    chore: remove importing font file

--- components/MaterialSymbol.vue
+++ components/MaterialSymbol.vue
@@ -1,13 +1,23 @@
 <script setup lang="ts">
 import type { MaterialSymbol } from "material-symbols";
 
-defineProps<{
-  icon: MaterialSymbol;
-}>();
+withDefaults(
+  defineProps<{
+    icon: MaterialSymbol;
+    size?: number;
+  }>(),
+  {
+    size: 24,
+  }
+);
 </script>
 
 <template>
-  <span class="material-symbols-rounded" v-html="icon"></span>
+  <span
+    class="material-symbols-rounded"
+    :style="{ height: `${size}px`, width: `${size}px`, fontSize: `${size}px` }"
+    v-html="icon"
+  ></span>
 </template>
 
 <style scoped></style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,7 +8,13 @@ export default defineNuxtConfig({
     head: {
       bodyAttrs: {
         class: "dark"
-      }
+      },
+      link: [
+        {
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
+        }
+      ]
     }
   },
   css: [
