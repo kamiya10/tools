f1e43c400c10bde37b5af03b035b1874f27ed475
Kamiya <kamiya@kamiya.app>
2024-07-27 20:46:08 +0800

    refactor: redirect (#2)

--- pages/bot.vue
+++
@@ -1,8 +0,0 @@
-<script setup lang="ts">
-navigateTo("https://top.gg/bot/707186246207930398", {
-  external: true,
-  replace: true,
-});
-</script>
-
-<template></template>
---
+++ server/routes/bot.ts
@@ -0,0 +1,3 @@
+export default defineEventHandler(async  (event) => {
+  await sendRedirect(event, "https://top.gg/bot/707186246207930398");
+})
